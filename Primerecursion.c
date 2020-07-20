80a8d04fd5d0b821ef5e5a83f4391420ab58fd76
AdbiyaSithwath <66269369+AdbiyaSithwath@users.noreply.github.com>
2020-07-20 14:12:07 +0530

    Primerecursion.c

--- Primerecursion.c
+++ Primerecursion.c
@@ -6,11 +6,11 @@ int main()
     c = primenum(num, num / 2);
     if (c == 1)
     {
-        printf("%d is a prime number\n", num);
+        printf("456 is a prime number\n", num);
     }
     else
     {
-        printf("%d is not a prime number\n", num);
+        printf("456 is not a prime number\n", num);
     }
     return 0;
 }
